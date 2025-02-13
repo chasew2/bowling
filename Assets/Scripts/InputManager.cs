cac725c8b41e3c646f3ab9364764387eef23b9c4
Chase <chasew13579@gmail.com>
2025-02-13 13:39:28 -0800

    Rough and smooth floor mats added

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+  
     public UnityEvent OnSpacePressed = new UnityEvent();
     void Update()
     {
@@ -18,5 +13,15 @@ public class InputManager : MonoBehaviour
         {
             OnSpacePressed?.Invoke();
         }
+          Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        OnMove?.Invoke(input);
     }
 }
